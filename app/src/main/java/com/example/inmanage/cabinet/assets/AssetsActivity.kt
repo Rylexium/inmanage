6bead926aeca52f951fa8600a4c243ae06ed03d0
rylexium <aleksei.azizov@mail.ru>
2023-02-28 23:48:08 +0400

    check point

---
+++ app/src/main/java/com/example/inmanage/cabinet/assets/AssetsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.inmanage.cabinet.assets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AssetsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_assets)
+    }
+}
