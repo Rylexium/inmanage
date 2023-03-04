5dd25c41e56be336d321dd8c190f5507ce4b8ef9
rylexium <aleksei.azizov@mail.ru>
2023-03-05 00:05:40 +0400

    add field in statisctics

--- app/src/main/java/com/example/inmanage/cabinet/list/StatisticsFragment.kt
+++ app/src/main/java/com/example/inmanage/cabinet/list/StatisticsFragment.kt
@@ -1,11 +1,17 @@
 package com.example.inmanage.cabinet.list
 
+import android.R
+import android.content.Context
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.inmanage.databinding.FragmentStatisticsBinding
+
 
 class StatisticsFragment: Fragment() {
     private var _binding: FragmentStatisticsBinding? = null
@@ -19,7 +25,15 @@ class StatisticsFragment: Fragment() {
         _binding = FragmentStatisticsBinding.inflate(inflater, container, false)
 
         initComponents()
+        applyEvents()
         return binding.root
+    }
+
+    private fun applyEvents() {
+        binding.textOfInterval.setOnClickListener {
+
+
+        }
     }
 
     private fun initComponents() {
