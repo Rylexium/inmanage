91f828ccbe4a6141e9ab98b3f7380ec7c83ef131
rylexium <aleksei.azizov@mail.ru>
2023-03-04 15:55:35 +0400

    add animation in cabinet

--- app/src/main/java/com/example/inmanage/cabinet/CabinetActivity.kt
+++ app/src/main/java/com/example/inmanage/cabinet/CabinetActivity.kt
@@ -10,6 +10,7 @@ import android.widget.ListView
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import com.example.inmanage.R
 import com.example.inmanage.cabinet.activity.AssetsActivity
@@ -30,13 +31,13 @@ class CabinetActivity : AppCompatActivity() {
 
     private var fragments = listOf(ReportsFragment(), AssetsFragment(), SchedulerFragment(), ProfileFragment())
     private val listOfAssets = mutableListOf("Баланс", "Активы", "Пассивы")
+    private var pastIndex = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cabinet)
         initComponents()
         applyEvents()
-        replaceFragment(fragments[1])
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
@@ -53,6 +54,7 @@ class CabinetActivity : AppCompatActivity() {
 
         bottomNavigationBar = findViewById(R.id.bottom_navigation)
         bottomNavigationBar.selectedItemId = R.id.button_balance
+        replaceFragment(fragments[pastIndex], pastIndex, 0)
     }
 
     private fun applyEvents() {
@@ -60,16 +62,20 @@ class CabinetActivity : AppCompatActivity() {
             it.isChecked = true
             when (it.itemId) {
                 R.id.button_reports -> {
-                    replaceFragment(fragments[0])
+                    replaceFragment(fragments[0], pastIndex, 0)
+                    pastIndex = 0
                 }
                 R.id.button_balance -> {
-                    replaceFragment(fragments[1])
+                    replaceFragment(fragments[1], pastIndex, 1)
+                    pastIndex = 1
                 }
                 R.id.button_scheduler -> {
-                    replaceFragment(fragments[2])
+                    replaceFragment(fragments[2], pastIndex, 2)
+                    pastIndex = 2
                 }
                 R.id.button_profile -> {
-                    replaceFragment(fragments[3])
+                    replaceFragment(fragments[3], pastIndex, 3)
+                    pastIndex = 3
                 }
             }
             false
@@ -97,8 +103,12 @@ class CabinetActivity : AppCompatActivity() {
         }
     }
 
-    private fun replaceFragment(fragment: Fragment){
+    private fun replaceFragment(fragment: Fragment, pastIndex : Int, previousIndex : Int){
         val transaction = supportFragmentManager.beginTransaction()
+        if(pastIndex < previousIndex)
+            transaction.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left)
+        else
+            transaction.setCustomAnimations(R.anim.slide_in_left, R.anim.slide_out_right)
         transaction.replace(R.id.fragment_container, fragment)
         transaction.commit()
     }
